0e065d7591826cc22867acfce108d7c897fed0d6
Thirusudan <thirusudan110@gmail.com>
2026-08-29 14:26:04 +0530

    include role details to user.java🚀

--- src/main/java/com/devstack/SmartDine/entity/user.java
+++ src/main/java/com/devstack/SmartDine/entity/user.java
@@ -3,11 +3,12 @@ package com.devstack.SmartDine.entity;
 
 import com.devstack.SmartDine.entity.enums.AuthProvider;
 import com.devstack.SmartDine.entity.enums.UserStatus;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EntityListeners;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.jmx.export.annotation.ManagedResource;
 
 import java.time.LocalDateTime;
 import java.time.LocalTime;
@@ -24,26 +25,81 @@ import java.util.UUID;
 @AllArgsConstructor
 @Builder
 public class user {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name="id", updatable = false, nullable = false)
     private UUID id;
+
+    @Column(name = "keycloak_id" , unique = true)
     private String keycloakId;
+
+    @Column(name="first_name",nullable= false,length=100)
     private String firstName;
+
+    @Column(name="last_name",nullable= false,length=100)
     private String lastName;
+
+    @Column(name="email",nullable= false, length=255 ,unique=true)
     private String email;
+
+    @Column(name="password_Hash")
     private String passwordHash;
+
+    @Column(name="phone",length = 20)
     private String phoneNumber;
+
+    @Column(name="profile_picture",columnDefinition = "TEXT")
     private String profilePictureUrl;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status" , nullable = false)
+    @Builder.Default
     private UserStatus status = UserStatus.PENDING_VERIFICATION;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "provider" , nullable = false)
+    @Builder.Default
     private AuthProvider provider = AuthProvider.LOCAL;
+
+    @Column(name="provider_id")
     private String providerId;
+
+    @Column(name="email_Verified" , nullable =false)
+    @Builder.Default
     private boolean emailVerified = false;
+
+    @Column(name="phone_Verified" , nullable =false)
+    @Builder.Default
     private boolean phoneVerified = false;
+
+    @Column(name="two_factor_enabled" , nullable =false)
+    @Builder.Default
     private boolean twoFactorEnabled = false;
+
+    @Column(name="last_login_at" , nullable =false)
     private LocalDateTime lastLoginAt;
 
+    @ManyToMany(fetch = FetchType.EAGER,cascade = {CascadeType.PERSIST,CascadeType.MERGE})
+    @JoinTable(
+            name= "user_roles",
+            joinColumns = @JoinColumn(name="user_id"),
+            inverseJoinColumns = @JoinColumn(name="role_id")
+    )
 
+
+
+    @Builder.Default
     private Set<Role> roles = new HashSet<>();
+
+    @CreatedDate
+    @Column(name= "created_at" , nullable = false, updatable = false)
     private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name= "updated_at" , nullable = false)
     private LocalTime updatedAt;
+
+    @Column(name= "deleted_at" )
     private LocalTime deletedAt;
 
     public void addRole(Role role){
